4bdb486d59f5b31e6497a9084a7ed4d97b32915f
Andrew Quaye <andyquayegh@outlook.com>
2018-12-25 05:57:13 +0100

    version2
    version2

---
+++ moonify/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+include("moonify.php"); //include the sdk
+
+$moonify = new Moonify(); //declare a new moonify instance
+
+$moonify->set("serviceID","3lgIBtTCTuDIUZNu8Xy5xnncGPYxH7Xzf5w_ICJW-iaras8kzkCE3pzMDcxv1jgYpr8E5_WHqLfVz5zNKJj7u3N3sfG25NrgeIeV2HiRo1Pk21Ee2CcvzOaetxScR8k2"); //set your private ID as authentication
+
+$session​ ​=​ $moonify->session([​])->open(); //open a monetization session
+
+if($moonify->error){
+    echo $moonify->error; //show error
+} else {
+    echo $session->integrationCode(); //add Javascript code to set the monetization on client-side
+}
+
+?>
